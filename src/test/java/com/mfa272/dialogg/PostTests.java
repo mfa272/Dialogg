600a5f8b80c8b17e48f971610ec037e5438e0805
mfa272 <mfazio272@gmail.com>
2024-04-16 16:27:54 +0200

    updated post test

--- src/test/java/com/mfa272/dialogg/PostTests.java
+++ src/test/java/com/mfa272/dialogg/PostTests.java
@@ -5,31 +5,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.mock.web.MockHttpSession;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL;INIT=CREATE SCHEMA IF NOT EXISTS DUMMY;",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=sa",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 public class PostTests {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void createPost() throws Exception{
+    void createPost() throws Exception {
 
-		mockMvc.perform(post("/register")
-        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-        .param("username", "username")
-        .param("email", "test@test.username")
-        .param("password", "password"))
-        .andExpect(status().is3xxRedirection())
-        .andExpect(view().name("redirect:/login"));
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username", "username")
+                .param("email", "test@test.username")
+                .param("password", "password").with(csrf()));
 
-        mockMvc.perform(post("/posts/new").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("username", "username").param("content", "this is a test post"))
-                .andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/posts"));
+        MvcResult result = mockMvc.perform(post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username", "username")
+                .param("password", "password")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/posts/new"))
+                .andReturn();
+
+        MockHttpSession session = (MockHttpSession) result.getRequest().getSession(false);
+
+        mockMvc.perform(post("/posts/new")
+                .session(session)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username", "username")
+                .param("content", "this is a test post")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/posts"));
     }
 }
