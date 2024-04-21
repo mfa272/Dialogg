3173c9d1b7cd2b6b4208eb11c62e75cd25fc4c2b
mfa272 <mfazio272@gmail.com>
2024-04-21 18:28:53 +0200

    removed unused import

--- src/main/java/com/mfa272/dialogg/services/AccountService.java
+++ src/main/java/com/mfa272/dialogg/services/AccountService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class AccountService {
