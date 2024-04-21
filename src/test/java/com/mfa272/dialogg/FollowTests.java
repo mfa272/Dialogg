ae0253919ba5614b2e53b32694b5413e471003fd
mfa272 <mfazio272@gmail.com>
2024-04-21 18:18:46 +0200

    removed unused imports

--- src/test/java/com/mfa272/dialogg/FollowTests.java
+++ src/test/java/com/mfa272/dialogg/FollowTests.java
@@ -1,8 +1,6 @@
 package com.mfa272.dialogg;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.LinkedHashMap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
