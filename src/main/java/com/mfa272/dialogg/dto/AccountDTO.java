db2856b06a9512bcc24041d4d21f0efbb5c1839c
mfa272 <mfazio272@gmail.com>
2024-04-23 16:32:54 +0200

    updated username regex

--- src/main/java/com/mfa272/dialogg/dto/AccountDTO.java
+++ src/main/java/com/mfa272/dialogg/dto/AccountDTO.java
@@ -19,7 +19,7 @@ public class AccountDTO {
     @Size(max = 16, message = "Maximum 16 characters", groups = { RegistrationForm.class })
     @Size(min = 6, message = "Minimum 6 characters", groups = { RegistrationForm.class })
     @NotBlank(message = "Username is required", groups = { RegistrationForm.class })
-    @Pattern(regexp = "^[\\S]*$", message = "Username must not contain whitespaces", groups = {
+    @Pattern(regexp = "^[a-zA-Z0-9._]+$", message = "Username contains unallowed characters", groups = {
             RegistrationForm.class })
     private String username;
 
