2d76341b96252a6a169506d3b05ebca0fd8b2358
mfa272 <mfazio272@gmail.com>
2024-04-23 16:29:12 +0200

    removed unused imports

--- src/main/java/com/mfa272/dialogg/controllers/AuthenticationController.java
+++ src/main/java/com/mfa272/dialogg/controllers/AuthenticationController.java
@@ -9,9 +9,6 @@ import com.mfa272.dialogg.services.AccountService.OperationResult;
 import com.mfa272.dialogg.services.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
