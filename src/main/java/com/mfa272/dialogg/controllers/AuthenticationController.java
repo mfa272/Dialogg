429dc35ccafcf7517ca928b1dae042b6ca1d498e
mfa272 <mfazio272@gmail.com>
2024-04-23 14:55:56 +0200

    removed unused imports

--- src/main/java/com/mfa272/dialogg/controllers/AuthenticationController.java
+++ src/main/java/com/mfa272/dialogg/controllers/AuthenticationController.java
@@ -19,10 +19,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.validation.Valid;
 import java.util.Optional;
 
 @Controller
